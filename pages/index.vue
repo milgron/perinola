e01c50d2d7eed3a7908a778349f85bcd6bd062e3
milgron <milgron@gmail.com>
2020-07-29 12:37:42 -0300

    first

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="main-container">
+      <div class="title-wrapper">
+        <h1 class="title">{{title}}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'perinola'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.main-container {
+  padding: 2rem;
+  border: 1px solid #ddd;
+  margin: 1rem;
+}
+</style>
